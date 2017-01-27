12c0afc0a5b1f8b8cd328c4c4047ab9bcc656a7f
Thomas Kipf <thomas.kipf@gmail.com>
2017-01-27 10:58:29 +0100

    Change default filter to localpool

--- kegra/train.py
+++ kegra/train.py
@@ -12,7 +12,7 @@ import time
 
 # Define parameters
 DATASET = 'cora'
-FILTER = 'chebyshev'  # 'chebyshev'
+FILTER = 'localpool'  # 'chebyshev'
 MAX_DEGREE = 2  # maximum polynomial degree
 SYM_NORM = True  # symmetric (True) vs. left-only (False) normalization
 NB_EPOCH = 200
