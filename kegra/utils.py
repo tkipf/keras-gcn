791667dc4cd197d9877db30031a129c4a838e1e3
Thomas Kipf <thomas.kipf@gmail.com>
2017-06-16 20:57:00 +0200

    Fix python 3.x compatibility for data preprocessing

--- kegra/utils.py
+++ kegra/utils.py
@@ -8,7 +8,7 @@ from scipy.sparse.linalg.eigen.arpack import eigsh, ArpackNoConvergence
 def encode_onehot(labels):
     classes = set(labels)
     classes_dict = {c: np.identity(len(classes))[i, :] for i, c in enumerate(classes)}
-    labels_onehot = np.array(map(classes_dict.get, labels), dtype=np.int32)
+    labels_onehot = np.array(list(map(classes_dict.get, labels)), dtype=np.int32)
     return labels_onehot
 
 
@@ -16,15 +16,15 @@ def load_data(path="data/cora/", dataset="cora"):
     """Load citation network dataset (cora only for now)"""
     print('Loading {} dataset...'.format(dataset))
 
-    idx_features_labels = np.loadtxt("{}{}.content".format(path, dataset), dtype=np.dtype(str))
+    idx_features_labels = np.genfromtxt("{}{}.content".format(path, dataset), dtype=np.dtype(str))
     features = sp.csr_matrix(idx_features_labels[:, 1:-2], dtype=np.float32)
     labels = encode_onehot(idx_features_labels[:, -1])
 
     # build graph
     idx = np.array(idx_features_labels[:, 0], dtype=np.int32)
     idx_map = {j: i for i, j in enumerate(idx)}
-    edges_unordered = np.loadtxt("{}{}.cites".format(path, dataset), dtype=np.int32)
-    edges = np.array(map(idx_map.get, edges_unordered.flatten()),
+    edges_unordered = np.genfromtxt("{}{}.cites".format(path, dataset), dtype=np.int32)
+    edges = np.array(list(map(idx_map.get, edges_unordered.flatten())),
                      dtype=np.int32).reshape(edges_unordered.shape)
     adj = sp.coo_matrix((np.ones(edges.shape[0]), (edges[:, 0], edges[:, 1])),
                         shape=(labels.shape[0], labels.shape[0]), dtype=np.float32)
